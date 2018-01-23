07b275d1ac5a347e9ba16ed10e145b876e016e5f
AlbertHerbst <albert.herbst@etuna.ntig.se>
2018-01-23 21:09:15 +0100

    Justeringar

---
+++ admin.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<!--[if lt IE 9 ]><html class="no-js oldie" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>Administratör</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="css/base.css">
+    <link rel="stylesheet" href="css/vendor.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" type="text/css" href="css/admin.css">
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script src="js/pace.min.js"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
+
+</head>
+
+<body id="top">
+
+    <!-- header
+    ================================================== -->
+    <header class="s-header">
+
+        <div class="header-logo">
+            <a class="site-logo" href="index.html">
+                <img src="images/logga_liten.png" alt="Homepage">
+            </a>
+        </div>
+
+        
+
+
+
+        <div id="loginwindow">
+        <form align="center" method="post" action="login.php" >
+             <?php include('errors.php'); ?>
+             <div>
+            <label>Användarnamn</label><br>
+                
+                <input name="username" class="full-width" type="email" placeholder="test@mailbox.com" id="sampleInput" required>
+            </div>       
+         
+            <div> 
+            <label>Lösenord</label><br>
+                
+                <input name="password" required class="full-width" type="password" placeholder="password" id="sampleInput">
+            </div>         
+           
+            <div>
+                <button  type="submit" name="login" class="knappjao">Logga In</button>
+            </div>
+            <p>Har du inte ett konto? <a href="register.php">Registrera dig</a></p>
+              
+           
+        </form>
+        </div>
+
+    </header> <!-- end s-header -->
+
+
+    <!-- home
+    ================================================== 
+    BAKGRUNDSBILD = data-image-src
+    -->
+    <section id="home" class="s-home target-section" data-parallax="scroll" data-image-src="http://blog.visme.co/wp-content/uploads/2017/07/50-Beautiful-and-Minimalist-Presentation-Backgrounds-030.jpg" data-natural-width=3000 data-natural-height=2000 data-position-y=center>
+
+            
+
+
+       
+
+    </section> <!-- end s-home -->
+
+
+   
+
+   
+
+    </footer> <!-- end footer -->
+
+
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale-pulse-out">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>
+
+</body>
+
+</html>
